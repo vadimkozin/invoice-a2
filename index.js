0e5fc1b238f22931f1bcaad4e030d695f65bfd44
Vadim Kozin <ulin.salma@yandex.ru>
2021-11-08 19:23:00 +0300

    second

--- index.js
+++ index.js
@@ -1,57 +1,142 @@
+require('dotenv').config()
 const fs = require('fs-extra')
 const path = require('path')
 const fn = require('./lib/func')
+const file = require('./lib/file')
 const act = require('./lib/act')
 const invoice = require('./lib/invoice')
+const Logging = require('./lib/logging').Logging
+const arh = require('./lib/arhiver')
+const mail = require('./lib/mail')
+const xlsx = require('./lib/xlsx')
+const error = require('./lib/error')
+const opts = require('minimist')(process.argv.slice(2), {
+  alias: { help: 'h', file: 'f', compress: 'c', send: 's', email: 'e' },
+})
+
+const nameProgramm = path.basename(process.argv[1])
+
+const help = `Creating pdf documents (invoice, act) and sending the result by e-mail.
+used: node ${nameProgramm} -f file.csv [-cs] [-e]
+-f (--file)     name source file(csv) 
+-c (--compress) compress result
+-s (--send)     sending compress result by e-mail
+-e (--email)    email recipient's address instead of the default address
+example:
+node ${nameProgramm} -f file.csv -cs --email=addr@mail.ru
+`
 
 global.appRoot = path.resolve(__dirname)
 const pathResult = `${appRoot}/result`
-const pathData = `${appRoot}/data`
+const pathLog = `${appRoot}/log`
+const pathZip = pathResult
+const fileLog = `${pathLog}/invoice.log`
 
-const fileMg = 'westcall-longdistance-2021-09.csv'
-const fileVz = 'westcall-intrazone-2021-09.csv'
-const fileNameMg = `${pathData}/${fileMg}`
-const fileNameVz = `${pathData}/${fileVz}`
+const log = new Logging(pathLog, fileLog)
 
-const main = (data) => {
-  const [result, totalSum] = fn.prepareData(data)
-
+const createDocuments = (result, cfg) => {
   const customersIds = Object.keys(result).sort()
 
-  customersIds.forEach(async (cid) => {
+  customersIds.forEach((cid) => {
     const it = result[cid]
     if (it.customer.type === 'u') {
-      const period = `${it.year}_${fn.padStart(it.month, 2, '0')}` // 2021_09
-      const storage = `${pathResult}/${period}` // ../result/2021_09
-      await fn.createDirectory(storage)
       const typeTraf = fn.getTrafficSymbol(it.trafficType)
-
-      const nameFileAct = fn.getNameOutputFile(storage, period, it.customer, typeTraf, 'act')
+      // prettier-ignore
+      const nameFileAct = fn.getNameOutputFile(cfg.storage, cfg.period, it.customer, typeTraf, 'act')
       act.createAct(it, nameFileAct)
-
-      const nameFileInvoice = fn.getNameOutputFile(storage, period, it.customer, typeTraf, 'invoice')
+      // prettier-ignore
+      const nameFileInvoice = fn.getNameOutputFile(cfg.storage, cfg.period, it.customer, typeTraf, 'invoice')
       invoice.createInvoice(it, nameFileInvoice)
     }
   })
-
-  console.log('total sum:', totalSum)
-
-
 }
 
-fn.readFile(fileNameMg, main)
-fn.readFile(fileNameVz, main)
+const run = async (cfg, data) => {
+  try {
+    // подготовка данных
+    const [result, totalSum] = fn.prepareData(data)
 
+    // создание документов
+    createDocuments(result, cfg)
+    log.add(`result: ${cfg.typeTraf.toUpperCase()}, total sum: ${totalSum.toFixed(2)}`, true)
 
-function one (data) {
-  const [result, totalSum] = fn.prepareData(data)
-  const it = result['28984']
-  const period = `${it.year}_${fn.padStart(it.month, 2, '0')}` // 2021_09
-  const storage = `${pathResult}/${period}` // ../result/2021_09
-  fn.createDirectory(storage)
-  const typeTraf = fn.getTrafficSymbol(it.trafficType)
-  const nameFileAct = fn.getNameOutputFile(storage, period, it.customer, typeTraf, 'act')
-  act.createAct(it, nameFileAct)
-  const nameFileInvoice = fn.getNameOutputFile(storage, period, it.customer, typeTraf, 'invoice')
-  invoice.createInvoice(it, nameFileInvoice)
-} 
+    // сжатие
+    if (opts.compress) {
+      const { bytes } = await arh.compressDirectory(`${cfg.storage}/`, cfg.fileZip, log)
+      log.add(`compress: ${path.basename(cfg.fileZip)}, ${fn.bytesToMb(bytes)} mb`, true)
+
+      // отправка результата
+      if (opts.send) {
+        try {
+          const { sendedTo, bytes } = await mail.sendEmail({ filename: cfg.fileZip, email: opts.email })
+          log.add(`mail: ${sendedTo}, ${fn.bytesToMb(bytes)} mb`, true)
+        } catch (e) {
+          log.add(`error: ${e}`, true)
+        }
+        log.add('.', true)
+      }
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return log.add(`error: ${err.message}`, true)
+    } else if (err instanceof error.ValidationError) {
+      return log.add(`error: ${err.message}`, true)
+    } else {
+      log.add(err.stack, true)
+      throw err
+    }
+  }
+}
+
+const main = async () => {
+  log.add(`cmd: ${process.argv.slice(2).join(' ')}`)
+
+  if (opts.help) {
+    console.log(help)
+  }
+
+  if (!opts.file) {
+    console.log(help)
+  } else {
+    log.add(`file: ${path.basename(opts.file)}`)
+    const period = fn.getPeriodFromNameFile(opts.file) // 2021_09
+
+    const cfg = {
+      filename: opts.file,
+      period: fn.getPeriodFromNameFile(opts.file), // 2021_09
+      typeTraf: fn.getTypeTrafficFromNameFile(opts.file), // mg | vz
+      storage: `${pathResult}/${period}`, // ../result/2021_09/
+      fileZip: `${pathZip}/${period}_wc.zip`, // ../result/2021_09/2021_09.zip
+      sheetNumber: 0, // номер листа в xls-файле с данными, если исходым является xls-файл
+      fieldSeparator: ';', // разделитель полей в csv-файле
+    }
+
+    try {
+      fs.ensureDirSync(cfg.storage)
+
+      const ext = fn.getFileExtension(opts.file)
+
+      if (ext === 'csv') {
+        run(cfg, await file.readFileCsv(cfg))
+      } else if (ext === 'xls') {
+        run(cfg, await xlsx.makeCsvFromXls(cfg))
+      } else {
+        log.add(`Неизвестное расширение файла: ${ext}`, true)
+      }
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        log.add(`error: ${err.message}`, true)
+      } else {
+        throw err
+      }
+    }
+  }
+}
+
+process.on('uncaughtException', (err) => {
+  log.add(`exception: ${err}`)
+  console.error(err.message)
+  process.exit(1)
+})
+
+main()
