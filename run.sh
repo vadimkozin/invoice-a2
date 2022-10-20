f3aeb4252bf1ba0b0f71d65efbcc0aba50efd5b0
Vadim Kozin <ulin.salma@yandex.ru>
2022-10-20 14:47:36 +0300

    changed dogovor number VZ for customer Naveton

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 YEAR=2022
-MONTH=05
+MONTH=09
 
-node ./index.js -aics -f ./sources/${YEAR}/westcall-longdistance-${YEAR}-${MONTH}.xls
+#node ./index.js -aics -f ./sources/${YEAR}/westcall-longdistance-${YEAR}-${MONTH}.xls
 node ./index.js -aics -f ./sources/${YEAR}/westcall-intrazone-${YEAR}-${MONTH}.xls
