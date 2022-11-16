5dd2b681eb4d022110e1648af75813910ecba686
Vadim Kozin <ulin.salma@yandex.ru>
2022-11-16 11:49:27 +0300

    create documents 2022_10

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 YEAR=2022
-MONTH=09
+MONTH=10
 
-#node ./index.js -aics -f ./sources/${YEAR}/westcall-longdistance-${YEAR}-${MONTH}.xls
+node ./index.js -aics -f ./sources/${YEAR}/westcall-longdistance-${YEAR}-${MONTH}.xls
 node ./index.js -aics -f ./sources/${YEAR}/westcall-intrazone-${YEAR}-${MONTH}.xls
